9c1165091353fca7840f1052729b7840c80f1028
apopovich1 <127229110+apopovich1@users.noreply.github.com>
2023-03-07 11:14:49 -0500

    Add files via upload

--- debugger.py
+++ debugger.py
@@ -5,12 +5,13 @@ def menu():
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
+    #menu set up
 
 def encoder(orgStr):
     newStr = ""
     for i in orgStr:
         if int(i) + 3 >= 10:
-            i = str(((int(i)+3) % 10))
+            i = str(((int(i)+3) % 10)) # allows for a wrap around if the number goes more than three
         else:
             i = str(int(i)+3)
         newStr = newStr + i
@@ -19,7 +20,7 @@ def decoder(orgStr):
     newStr = ""
     for i in orgStr:
         if int(i) - 3 < 0:
-            i = str(10 + (int(i)-3))
+            i = str(10 + (int(i)-3)) # goes the other way and add the negative number to ten
         else:
             i = str(int(i)-3)
         newStr = newStr + i
@@ -39,3 +40,4 @@ if __name__ == "__main__":
             print(f"The encoded password is {pswd}, and the original password is {input2} ")
         if input1 == 3:
             bool = False
+            #resets the loop
